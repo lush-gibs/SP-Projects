f49b74ece0d180ff60db3893f7df1abb528ad85a
lush-gibs <143350957+lush-gibs@users.noreply.github.com>
2024-10-24 17:54:34 +0300

    Add files via upload

--- GradeCalculator.cpp
+++ GradeCalculator.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <numeric>
 #include <iomanip>
-
-
+#include <cstdlib> 
+#include <ctime>
 
 using namespace std;
-
 class Person {
 private:
     string firstName, surName;
     double exam, grade;
     vector<double> HW;
-
 public:
     Person();
     Person(string firstName, string surName, double exam, double grade, vector<double> HW);
@@ -21,9 +20,8 @@ public:
     Person& operator= (const Person& other);
     friend istream& operator>>(istream& input, Person& p);
     friend ostream& operator<<(ostream& out, Person& p);
-    double calcFinalGrade();
+    double calcFinalGrade(int n);
 };
-
 Person::Person() {
     firstName = "";
     surName = "";
@@ -31,10 +29,7 @@ Person::Person() {
     grade = 0;
     HW = {};
 }
-
-Person::Person(string firstName, string surName, double exam, double grade, vector<double> HW)
-    : firstName(firstName), surName(surName), exam(exam), grade(grade), HW(HW) {}
-
+Person::Person(string firstName, string surName, double exam, double grade, vector<double> HW) : firstName(firstName), surName(surName), exam(exam), grade(grade), HW(HW) {}
 Person::Person(const Person& other) {
     firstName = other.firstName;
     surName = other.surName;
@@ -42,9 +37,7 @@ Person::Person(const Person& other) {
     grade = other.grade;
     HW = other.HW;
 }
-
 Person::~Person() {}
-
 Person& Person::operator=(const Person& other) {
     if (this != &other) {
         firstName = other.firstName;
@@ -55,58 +48,120 @@ Person& Person::operator=(const Person& other) {
     }
     return *this;
 }
+double Median(vector<double>& list) {
+    double Median;
 
-double Person::calcFinalGrade() {
-    double Average = 0;
-    double finalGrade;
-    for (int i = 0; i < HW.size(); i++) {
-        Average += HW[i];
+    int length = list.size();
+
+    if(length == 0) return 0;
+
+    sort(list.begin(),list.end());
+
+    if (length % 2 == 0) {
+        int mid = length / 2 ;
+        int mid1 = mid - 1;
+
+        Median = (list[mid] + list[mid1])/2;
     }
-    Average /= HW.size();
-    finalGrade = 0.4 * Average + 0.6 * exam;
+    else Median = list[length / 2];
+    
+    return Median;
+}
+
+double Person::calcFinalGrade(int n) {
+    double finalGrade = 0;
+
+    if (n == 1){
+        double average = accumulate(HW.begin(), HW.end(), 0.0)/HW.size();
+        finalGrade = 0.4 * average + 0.6 * exam;
+    } 
+    else if (n == 2)
+        finalGrade = 0.4 * Median(HW) + 0.6 * exam;
+
     return finalGrade;
 }
-
 istream& operator>>(istream& input, Person& p) {
-    cout << "Please enter student's name and surname: ";
+    cout << "\nPlease enter student's name and surname: " ;
     input >> p.firstName >> p.surName;
-    cout << "Please input how many homeworks were submitted by " << p.firstName << ": ";
-    int n;
-    input >> n;
-    for (int i = 0; i < n; i++) {
-        cout << "Input Homework " << i + 1 << " score: ";
-        double score;
-        input >> score;
-        p.HW.push_back(score);
+    
+    int n = 0;
+    int i = 0;                                                                           // variable i to track number of homework request prompts
+    do
+    {
+        cout << ((i == 0)? "\n1.Add Homework\n2.No Homework\nPress '1' to Add OR '2' to Skip: " : 
+        "\nPress '1' to continue adding homework OR '2' to finish: " );                //prompt to ask User if there is homework or notcle
+        input >> n;
+
+        if(i == 0 & n == 2)         // put zero in vector when no homework is  selected
+            p.HW.push_back(0.00);
+        
+        if(n == 1)
+        {
+            cout << "\n1. Input Homework Score\n2. Randomize Score\nInput option '1' or '2': ";
+            int q;
+            input >> q;
+        
+            double hw;
+
+            if (q == 1)
+            {
+                cout << "Input Homework " << i + 1 << ": ";
+                input >> hw;
+            }
+            else if (q == 2)
+            {   
+                hw = rand() % 100 + 1.00;           // generates random number for homework between 1 and 100
+                cout << hw << " generated" << endl;
+            }
+
+            i++;
+            p.HW.push_back(hw);                                           
+                
+        }
     }
-    cout << "Please enter student's exam score: ";
-    input >> p.exam;
+    while(n != 2);
+    
+    cout << "1. Input Exam Score\n 2. Generate exam Score\n Please input option 1 or 2: ";
+    int s;
+    input >> s;
+
+    if (s == 1)
+    {
+        cout << "Please enter student's exam score: ";
+        input >> p.exam;
+    }
+    else if (s == 2)
+    {
+        p.exam = rand() % 100 + 1;
+        cout << p.exam << " generated" << endl << endl;
+    }
+    
     return input;
 }
-
 ostream& operator<<(ostream& out, Person& p) {
     cout.setf(ios::left);
     out.width(20);
     out << p.firstName;
     out.width(20);
     out << p.surName;
-    out.width(20);
-    out << fixed << setprecision(2) << p.calcFinalGrade();
 
     return out;
 }
-
 int main() {
-    cout << "Please input number of students to calculate Final Grade based on average: ";
+    srand(time(0));
+
+    cout << "Please input number of students: ";
     int n;
     cin >> n;
 
-    vector<Person> arr;
+    cout << "\nCalculate Grade with:\n1. Average\n2. Median\ninput 1 for average and 2 for median: " ;
+    int m;
+    cin >> m;
 
-    for (int i = 0; i < n; i++) {
-        Person student;
+    vector<Person> arr(n);
+
+    for (Person& student : arr)  {
         cin >> student;
-        arr.push_back(student);
     }
 
     cout.setf(ios::left);
@@ -115,13 +170,14 @@ int main() {
     cout.width(20);
     cout << "Surname";
     cout.width(20);
-    cout << "Final_point(Aver.)" << endl;
+    cout << ((m == 1) ? "Final_point(Aver.)" : "Final_point(Median)")  << endl;
     cout << "------------------------------------------------------------" << endl;
 
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << endl;
+    for (Person& student : arr) {
+        cout << student;
+        cout.width(20);
+        cout << fixed << setprecision(2) << student.calcFinalGrade(m) << endl;
     }
 
     return 0;
 }
-
