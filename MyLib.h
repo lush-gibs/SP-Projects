65e22874bc1f57505f6d7723414c44be875f73fc
lush-gibs <143350957+lush-gibs@users.noreply.github.com>
2024-11-01 16:18:48 +0000

    Add files via upload
    Third commit 
    
    Updated features

---
+++ MyLib.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <iomanip>
+#include <cstdlib> 
+#include <ctime>
+#include<fstream>
+#include <algorithm>
+#include <sstream>
+
+using std::string;
+using std::vector; 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::sort;
+using std::right;
+using std:: left;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::ios;
+using std::istream;
+using std::ostream;
+using std::accumulate;
+using std::stringstream;
